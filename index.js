540f22bce4e1433ae9fd62f1ac2e9a09629b9dd6
Ayoola <johnsonayoolaojo@gmail.com>
2025-10-17 12:17:18 +0100

    update

---
+++ index.js
@@ -0,0 +1,84 @@
+// Data Types 
+//  - Strings 
+//  -Numbers 
+//  - Booleans 
+//  - Undefined 
+//     - Null
+//     - Symbol
+//     - BigInt
+
+
+
+const myName = 'Ayoola'
+const anotherName = ' Adams'
+console.log (myName)
+
+doubleNames = myName + anotherName
+
+console.log(doubleNames)
+
+const num1 = '10'
+console.log(myName + num1)
+
+
+
+isStudent = false 
+console.log(isStudent)  
+
+
+let fruit = 'mango';
+console.log(fruit)
+
+
+
+ let doneWithJavascript = null;
+
+
+    console.log(typeof(doneWithJavascript)) //typeof doneWithJavascript
+    console.log(typeof(100)) //typeof doneWithJavascript
+    console.log(typeof(myName)) //typeof doneWithJavascript
+    console.log(typeof(isStudent)) //typeof doneWithJavascript
+
+    const language = 'JavaScript'
+    const difficultyLevel = 8;
+    const isFun = true;
+    let nextLesson;
+    const completedLesson = null;
+
+    console.log(language)
+    console.log(difficultyLevel)
+    console.log(isFun)
+    console.log(nextLesson)
+    console.log(completedLesson)
+
+    const sentence = language + ' is fun to learn' 
+    
+const sentence1 = `${language} is  very fun to learn`
+console.log(sentence1)
+console.log(sentence)
+
+const a = 10
+const b = 5
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+console.log(a ** b) // exponentiation 10^5
+
+5 ==='5' 
+
+10 !== 5 
+
+
+const age = 25;
+const isDataslidStudent = true;
+
+console.log(age >30 && isDataslidStudent)
+
+// console.log(age > 18 && isStudent);  // true (both true)
+// console.log(age < 18 || isStudent);  // true (one true)
+// console.log(!isStudent);             // false (reverses true to false)
+
+
